0978dee02d52fab7a80b30cb34e755eb5c279ba8
B Phan <bphan@Bs-MacBook-Pro.local>
2023-04-05 19:34:14 -0600

    completed Q3

--- A4Q3Embed.py
+++ A4Q3Embed.py
@@ -4,11 +4,52 @@ import sys
 #############################################################################
 
 ############################ Class ############################
-class Database:
-    pass
+class SongDatabase:
+    def __init__(self, database_name, port_number):
+        self.client = MongoClient('localhost', port_number)
+        self.db = self.client[database_name]
+    
+    def close(self):
+        self.client.close()
+    
+    def solve_A4Q3Embed(self):
+        '''
+            Just like in A4Q3Norm.py except we don't have to join the documents from 2 tables anymore.
+        '''
+        pipeline = [
+            {
+                "$unwind": "$recordings"
+            },
+
+            {
+                "$group": {
+                    "_id": {
+                        "_id": "$_id",
+                        "songwriter_id": "$songwriter_id"
+                    },
+                    "total_length": {
+                        "$sum": "$recordings.length"
+                    }
+                }
+            },
+
+            {
+                "$project": {
+                    "_id": "$_id._id",
+                    "total_length": 1,
+                    "songwriter_id": "$_id.songwriter_id"
+                }
+            }
+        ]
+        result = self.db.SongwritersRecordings.aggregate(pipeline)
+        for songwriter in result:
+            print(songwriter)
 ###############################################################
 
 ############################ Styles ##########################
 if __name__ == "__main__":
-    pass
+    port_number = int(sys.argv[1])
+    A4dbEmbed = SongDatabase("A4dbEmbed", port_number)
+    A4dbEmbed.solve_A4Q3Embed()
+    A4dbEmbed.close()
 ##############################################################
